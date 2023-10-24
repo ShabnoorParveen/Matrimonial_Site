331e3ac237b9ab2ada8b4c2e7feedd55f10e8e9a
Shabnoor Parveen <shabnoorparveen1999@gmail.com>
2023-10-24 18:13:25 +0530

    sec 5 update

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ function googleTranslateElementInit() {
     var googleAttribution = document.querySelector(".goog-logo-link");
     googleAttribution.style.display = "none";
 }
+
+
+//sec-5-yourStory dynamic gradeint 
+
+const changeGradientButton = document.getElementById('changeGradient');
+    const root = document.documentElement;
+
+    changeGradientButton.addEventListener('click', () => {
+      const startColor = '#' + Math.floor(Math.random()*16777215).toString(16); // Random color
+      const endColor = '#' + Math.floor(Math.random()*16777215).toString(16); // Random color
+
+      root.style.setProperty('--gradient-start', startColor);
+      root.style.setProperty('--gradient-end', endColor);
+    });
