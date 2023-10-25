ab5aed5cff924f5e78a402971f4bd2e4c806cd06
Shabnoor Parveen <shabnoorparveen1999@gmail.com>
2023-10-25 17:04:08 +0530

    updated

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ function googleTranslateElementInit() {
 }
 
 
-//sec-5-yourStory dynamic gradeint 
+// sec-5-yourStory dynamic gradeint 
 
-const changeGradientButton = document.getElementById('changeGradient');
+document.addEventListener('DOMContentLoaded', function() {
+    const changeGradientButton = document.getElementById('changeGradient');
     const root = document.documentElement;
 
     changeGradientButton.addEventListener('click', () => {
-      const startColor = '#' + Math.floor(Math.random()*16777215).toString(16); // Random color
-      const endColor = '#' + Math.floor(Math.random()*16777215).toString(16); // Random color
+        const startColor = '#' + Math.floor(Math.random()*16777215).toString(16); // Random color
+        const endColor = '#' + Math.floor(Math.random()*16777215).toString(16); // Random color
 
-      root.style.setProperty('--gradient-start', startColor);
-      root.style.setProperty('--gradient-end', endColor);
+        root.style.setProperty('--gradient-start', startColor);
+        root.style.setProperty('--gradient-end', endColor);
     });
+});
